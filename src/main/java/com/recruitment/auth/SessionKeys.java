2041a3b5cebc29d1da2bdd16fdcb13a6fcb15fc7
SarraBoussaid <sarrouraboussaid025@gmail.com>
2026-07-28 09:07:50 +0100

    Add files via upload

---
+++ src/main/java/com/recruitment/auth/SessionKeys.java
@@ -0,0 +1,10 @@
+package com.recruitment.auth;
+
+public final class SessionKeys {
+    public static final String USER_ID = "userId";
+    public static final String ROLE = "role";
+    public static final String COMPANY_ID = "companyId";
+    public static final String CANDIDATE_ID = "candidateId";
+
+    private SessionKeys() {}
+}
